45c4505a69d27482b011b947acaabe474b3fe7b2
Daniel <daniel.bailey@grappleIT.co.uk>
2018-09-03 20:15:21 +0100

    Dark theme v1 Fixing migration to androidx Dark theme styling File details fragment tab text colour File list overflow menu background colour
    Signed-off-by: Daniel Bailey <daniel.bailey@grappleIT.co.uk>
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/MainApp.java
+++ src/main/java/com/owncloud/android/MainApp.java
@@ -90,6 +90,7 @@ import javax.inject.Inject;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 import androidx.multidex.MultiDexApplication;
@@ -188,6 +189,7 @@ public class MainApp extends MultiDexApplication implements
     @SuppressFBWarnings("ST")
     @Override
     public void onCreate() {
+        setAppTheme(preferences.getTheme());
         super.onCreate();
 
         registerActivityLifecycleCallbacks(new ActivityInjector());
@@ -735,5 +737,13 @@ public class MainApp extends MultiDexApplication implements
     @Override
     public AndroidInjector<BroadcastReceiver> broadcastReceiverInjector() {
         return dispatchingBroadcastReceiverInjector;
+    }
+
+    public static void setAppTheme(Boolean darkTheme) {
+        if (darkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
 }
